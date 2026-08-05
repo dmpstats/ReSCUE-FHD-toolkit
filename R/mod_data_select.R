4ed62d4d39d5a8e56e8c66cf5f4a884d0067b4f6
callumjclarke <cjosephclarke@outlook.com>
2026-08-05 16:14:37 +0100

    Added warning toast for too much data

--- R/mod_data_select.R
+++ R/mod_data_select.R
@@ -623,9 +623,24 @@ mod_data_select_server <- function(
 			removeModal()
 		})
 
-		# ----- Merge the user-uploaded data -----------
-		# The user can upload a dataset at any point. Reactively merge
-		# the user-uploaded data with the main metadata table. This allows the user to select their own datasets for analysis.
+		# ----- Too-much-data toast ------------
+		# If over 10 datasets are selected, show a warning toast that the analysis will only analyze the first 10 datasets.
+		observeEvent(selected_ids(), {
+			if (length(selected_ids()) > 10) {
+				bslib::showToast(
+					bslib::toast(
+						header = "Too many datasets selected",
+						icon = bsicons::bs_icon("exclamation-triangle-fill"),
+						"Only the first 10 datasets will be analyzed. Please deselect some datasets if you want to analyze fewer than 10.",
+						type = "warning",
+						id = "too_many_datasets_toast"
+					)
+				)
+			} else {
+				bslib::hide_toast("too_many_datasets_toast")
+			}
+		})
+
 
 		# ── Navigation ───────────────────────────────────────────────────────────
 		outputs <- reactiveValues()
